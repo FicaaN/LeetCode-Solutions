065982b573f3baabe46c88efaa992bf12c653c8c
FilipN <fiknikolic@gmail.com>
2024-09-20 18:08:40 +0200

    0899. Solution Added

---
+++ 🔴 Hard Problems/0899. Orderly Queue.py
@@ -0,0 +1,7 @@
+class Solution:
+    def orderlyQueue(self, s: str, k: int) -> str:
+        if k > 1:
+            return "".join(sorted(s))
+        
+        return min([s[i:] + s[:i] for i in range(len(s))])
+        
