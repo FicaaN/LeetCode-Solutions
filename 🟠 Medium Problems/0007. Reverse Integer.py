3a0e8f770428264e13586a84af4b2f66a52d4ada
FilipN <fiknikolic@gmail.com>
2024-09-20 18:11:11 +0200

    0007. Solution Added

---
+++ 🟠 Medium Problems/0007. Reverse Integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        if x >= 0:
+            reversed_x = int(str(x)[::-1])
+        else:
+            reversed_x = -1 * int(str(abs(x))[::-1])
+        
+        if reversed_x > 2 ** 31 - 1 or reversed_x < -2 ** 31:
+            return 0
+        
+        return reversed_x
