51767ef90a5991e0ad531c6dc2837624464234f9
FilipN <fiknikolic@gmail.com>
2024-09-26 14:26:53 +0200

    2798. Solution Added

---
+++ 🟢 Easy Problems/2798. Number of Employees Who Met the Target.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        result = 0
+        
+        for i in hours:
+            if i >= target:
+                result += 1
+        
+        return result
