8dfc75a6c203fd0e0d4993bff18f79f62f87658d
FilipN <fiknikolic@gmail.com>
2024-10-01 13:24:32 +0200

    1720. Solution Added

---
+++ 🟢 Easy Problems/1720. Decode XORed Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        output = [first]
+
+        for number in encoded:
+            output.append(output[-1] ^ number)
+        
+        return output
