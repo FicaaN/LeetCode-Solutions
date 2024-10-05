b58decfa215c073627d8200b29fd15483a21c8e7
FilipN <fiknikolic@gmail.com>
2024-10-05 13:24:33 +0200

    2974. Solution Added

---
+++ 🟢 Easy Problems/2974. Minimum Number Game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberGame(self, nums: List[int]) -> List[int]:
+        arr = []
+        nums.sort()
+
+        while nums:
+            alice = nums.pop(0)
+            bob = nums.pop(0)
+            arr.append(bob)
+            arr.append(alice)
+        
+        return arr
