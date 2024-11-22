a077d92c35350676361df8c9bd8e145ac73e832a
FilipN <fiknikolic@gmail.com>
2024-11-22 17:33:10 +0100

    2427. Solution Added

---
+++ 🟢 Easy Problems/2427. Number of Common Factors.py
@@ -0,0 +1,10 @@
+class Solution:
+    def commonFactors(self, a: int, b: int) -> int:
+
+        common_factors = 0
+
+        for i in range(1, min(a, b) + 1):
+            if a % i == 0 and b % i == 0:
+                common_factors += 1
+        
+        return common_factors
