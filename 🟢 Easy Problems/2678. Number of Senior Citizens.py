1a8f0f390cee8399c20e7fb4e76c87504bf19e75
FilipN <fiknikolic@gmail.com>
2024-11-26 18:47:12 +0100

    2678. Solution Added

---
+++ 🟢 Easy Problems/2678. Number of Senior Citizens.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        counter = 0
+        for person in details:
+            if int(person[11:13]) > 60:
+                counter += 1
+        
+        return counter
+
+        # return sum(1 for person in details if int(person[11:13]) > 60)
