5ca0656274b7ecb75cae52c9a827a0cb5bbe31d8
FilipN <fiknikolic@gmail.com>
2025-01-10 17:22:25 +0100

    1837. Solution Added

---
+++ 🟢 Easy Problems/1837. Sum of Digits in Base K.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumBase(self, n: int, k: int) -> int:
+        output = 0
+
+        while n > 0:
+            output += n % k
+            n //= k
+        
+        return output
